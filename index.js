99094f0e122cc2058763361fd662f7ce34850c8d
Vedvrat Sharma <vsharma@xactlycorp.com>
2017-06-12 11:46:54 +0530

    [UPDATE APP]
    Trigger Travis Build

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './src/App';
 
-function tick() {
-  ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import React, {Component} from 'react';
 export default class App extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            date: new Date()
+        };
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.setState({date: new Date()}),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
     }
     render() {
         return (
             <div>
               <h1>Hello, world!</h1>
-              <h2>It is {new Date().toLocaleTimeString()}.</h2>
+              <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         );
     }
